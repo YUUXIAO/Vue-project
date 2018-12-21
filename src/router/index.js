faf008a1f74c97620f7c8c1f98792bc578838078
yuxiao <1057305789@qq.com>
2018-12-21 18:14:41 +0800

    'router'

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-// const _import = require('./_import_' + process.env.NODE_ENV)
+const _import = require('./_import_' + process.env.NODE_ENV)
 
 Vue.use(Router)
 
 const router = [
-  // {
-  //   path: '/',
-  //   name: 'login',
-  //   component: _import('login')
-  // }
-  // {
-  //   path: '/',
-  //   name: 'HelloWorld',
-  //   component: _import('helloWorld')
-  // }
   {
     path: '/',
     name: 'Home',
-    component: () => import('@/views/login')
+    component: _import('Layout/HeaderNav')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: _import('login')
+  },
+  {
+    path: '/page1',
+    name: 'page1',
+    component: _import('page1')
+  },
+  {
+    path: '/page2',
+    name: 'page2',
+    component: _import('page2')
+  },
+  {
+    path: '/page3',
+    name: 'page3',
+    component: _import('page3')
   }
 ]
 
